8a8abda6ea9f94aeaeb2a02ce02e1e92d4d12846
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-08-17 20:09:28 -0700

    created logic for database functionality

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,29 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  // console.error('putDb not implemented')
+  const contactDb = await openDB("contact", 1);
+  const tx = contactDb.transaction("contact", "readwrite");
+  const store = tx.objectStore("contact"); 
+
+  const request = store.add(content); 
+
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+    // console.error('getDb not implemented')
+    const contactDb = await openDB("contact", 1); 
+    const tx = contactDb.transaction("contact", "readonly");
+    const store = tx.object("contact"); 
+    const request = store.getAll();
+
+    const result = await request; 
+
+    return result;
+  };
 
 initdb();
