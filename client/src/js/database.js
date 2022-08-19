13debe8940916428b31477017dadfc1be1b32abf
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-08-19 15:12:47 -0700

    Changed names to db variables

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,11 +15,11 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   // console.error('putDb not implemented')
-  const contactDb = await openDB("contact", 1);
-  const tx = contactDb.transaction("contact", "readwrite");
-  const store = tx.objectStore("contact"); 
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate"); 
 
-  const request = store.add(content); 
+  const request = store.add({ value: content }); 
 
   const result = await request;
   console.log('🚀 - data saved to the database', result);
@@ -28,9 +28,9 @@ export const putDb = async (content) => {
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
     // console.error('getDb not implemented')
-    const contactDb = await openDB("contact", 1); 
-    const tx = contactDb.transaction("contact", "readonly");
-    const store = tx.object("contact"); 
+    const jateDb = await openDB("jate", 1); 
+    const tx = jateDb.transaction("jate", "readonly");
+    const store = tx.objectStore("jate"); 
     const request = store.getAll();
 
     const result = await request; 
